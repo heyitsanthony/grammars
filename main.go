54602fffa1a41b4cde619cc630fad54a86026741
Anthony Romano <heyitsanthony@users.noreply.github.com>
2017-12-18 01:34:03 -0800

    language for /proc/diskstats

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 //go:generate peg -strict crontab/crontab.peg
+//go:generate peg -strict diskstats/diskstats.peg
 //go:generate peg -strict golang/golang.peg
 //go:generate peg -strict group/group.peg
 //go:generate peg -strict offside/offside.peg
@@ -19,6 +20,7 @@ import (
 	"os"
 
 	"github.com/heyitsanthony/grammars/crontab"
+	"github.com/heyitsanthony/grammars/diskstats"
 	"github.com/heyitsanthony/grammars/golang"
 	"github.com/heyitsanthony/grammars/group"
 	"github.com/heyitsanthony/grammars/offside"
@@ -42,30 +44,32 @@ type g interface {
 	PrintSyntaxTree()
 }
 
-func newCrontab(s string) g { return &crontab.Grammar{Buffer: s, Pretty: true} }
-func newGo(s string) g      { return &golang.Grammar{Buffer: s, Pretty: true} }
-func newGroup(s string) g   { return &group.Grammar{Buffer: s, Pretty: true} }
-func newOffside(s string) g { return &offside.Grammar{Buffer: s, Pretty: true} }
-func newPasswd(s string) g  { return &passwd.Grammar{Buffer: s, Pretty: true} }
-func newPeg(s string) g     { return &peg.Grammar{Buffer: s, Pretty: true} }
-func newPeg1(s string) g    { return &peg1.Grammar{Buffer: s, Pretty: true} }
-func newPopl04(s string) g  { return &popl04.Grammar{Buffer: s, Pretty: true} }
-func newRFC1459(s string) g { return &rfc1459.Grammar{Buffer: s, Pretty: true} }
-func newRFC2812(s string) g { return &rfc2812.Grammar{Buffer: s, Pretty: true} }
-func newShadow(s string) g  { return &shadow.Grammar{Buffer: s, Pretty: true} }
+func newCrontab(s string) g   { return &crontab.Grammar{Buffer: s, Pretty: true} }
+func newDiskstats(s string) g { return &diskstats.Grammar{Buffer: s, Pretty: true} }
+func newGo(s string) g        { return &golang.Grammar{Buffer: s, Pretty: true} }
+func newGroup(s string) g     { return &group.Grammar{Buffer: s, Pretty: true} }
+func newOffside(s string) g   { return &offside.Grammar{Buffer: s, Pretty: true} }
+func newPasswd(s string) g    { return &passwd.Grammar{Buffer: s, Pretty: true} }
+func newPeg(s string) g       { return &peg.Grammar{Buffer: s, Pretty: true} }
+func newPeg1(s string) g      { return &peg1.Grammar{Buffer: s, Pretty: true} }
+func newPopl04(s string) g    { return &popl04.Grammar{Buffer: s, Pretty: true} }
+func newRFC1459(s string) g   { return &rfc1459.Grammar{Buffer: s, Pretty: true} }
+func newRFC2812(s string) g   { return &rfc2812.Grammar{Buffer: s, Pretty: true} }
+func newShadow(s string) g    { return &shadow.Grammar{Buffer: s, Pretty: true} }
 
 var grammars = map[string](func(string) g){
-	"crontab": newCrontab,
-	"go":      newGo,
-	"group":   newGroup,
-	"offside": newOffside,
-	"passwd":  newPasswd,
-	"peg":     newPeg,
-	"peg1":    newPeg1,
-	"popl04":  newPopl04,
-	"rfc1459": newRFC1459,
-	"rfc2812": newRFC2812,
-	"shadow":  newShadow,
+	"crontab":   newCrontab,
+	"diskstats": newDiskstats,
+	"go":        newGo,
+	"group":     newGroup,
+	"offside":   newOffside,
+	"passwd":    newPasswd,
+	"peg":       newPeg,
+	"peg1":      newPeg1,
+	"popl04":    newPopl04,
+	"rfc1459":   newRFC1459,
+	"rfc2812":   newRFC2812,
+	"shadow":    newShadow,
 }
 
 func do(g g) error {
